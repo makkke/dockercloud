6de360c75ca1f35b7808bc211d04a068d07fde22
Viatcheslav Ivanov <slava.eth@gmail.com>
2016-04-21 23:18:07 -0700

    Add to ci (#5)
    chore(): add project to circleci

--- test/dockercloud.spec.js
+++ test/dockercloud.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import DockerCloud from '../src/dockercloud'
 
 describe('DockerCloud class', () => {
-  describe('extractUuid function', () => {
+  describe('extractUuid()', () => {
     it('should extract uuid from resource uri', () => {
       const dockercloud = new DockerCloud()
       const resourceUri = '/api/app/v1/action/7c42003e-eb39-4adc-b5b9-cbb7607fc698/'
